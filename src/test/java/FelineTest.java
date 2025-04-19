a1d26c75cb421ace5eafa7730bd63d7e6eb5919a
A.Kalnina <a.kalnina@russianpost.ru>
2025-04-19 20:30:46 +0300

    new branch

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,33 @@
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
+
+public class FelineTest {
+
+    @Test
+    public void getKittensForFelineReturnsOneTest(){
+        Feline feline = new Feline();
+        Assert.assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensWithFiveForFelineReturnsFiveTest(){
+        Feline feline = new Feline();
+        Assert.assertEquals(5, feline.getKittens(5));
+    }
+
+    @Test
+    public void getFamilyForFelineReturnsCats(){
+        Feline feline = new Feline();
+        Assert.assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getFoodForFelineReturnsMeat() throws Exception {
+        Feline feline = new Feline();
+        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
+    }
+
+}
